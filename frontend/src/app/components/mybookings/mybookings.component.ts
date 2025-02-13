fba83c6ce5469b5650ad0103cf35b126261b9bd4
mohammedAljadd <aljadd3@gmail.com>
2025-02-13 13:21:06 +0100

    feat: Implement cancel booking functionality

--- frontend/src/app/components/mybookings/mybookings.component.ts
+++ frontend/src/app/components/mybookings/mybookings.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { UserBooking } from '../../model/class/UserBooking';
 import { UserbookingsService } from '../../services/userbookings.service';
 
@@ -11,9 +11,13 @@ import { UserbookingsService } from '../../services/userbookings.service';
 })
 export class MybookingsComponent implements OnInit {
   
+  constructor(private cdr: ChangeDetectorRef){};
+  
   userBookings: UserBooking[] = [];
 
   userbookingsService = inject(UserbookingsService);
+
+  
 
   ngOnInit(): void {
     // Get the token from localStorage or other places
@@ -37,6 +41,20 @@ export class MybookingsComponent implements OnInit {
 
 
   cancelBooking(bookingId: number): void {
-    console.log("Testing PRs");
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      this.userbookingsService.cancelBooking(token, bookingId).subscribe({
+        next: response  => {
+          this.userBookings = this.userBookings.filter(booking => booking.bookingId !== bookingId); // important, we have to make changes to component to be able to use cdr
+          this.cdr.detectChanges();
+
+        },
+        error: error => {
+          console.error('Error fetching user bookings:', error);
+        }
+      });
+    } else {
+      console.log('No token found');
+    }
   }
 }
