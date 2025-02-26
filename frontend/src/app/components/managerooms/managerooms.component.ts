593d89ab46fd589f142613675dc4ae6f4129050f
mohammedAljadd <aljadd3@gmail.com>
2025-02-26 16:37:16 +0100

    add css to the modal

--- frontend/src/app/components/managerooms/managerooms.component.ts
+++ frontend/src/app/components/managerooms/managerooms.component.ts
@@ -63,7 +63,6 @@ export class ManageroomsComponent implements OnInit{
     this.equipmentService.fetAllEquipements(token, roomId).subscribe({
       next: (data) => {
         this.allEquipments = data;
-        console.log("Equipment : " + this.allEquipments[5]);
       },
       error: (error) => console.error('Error fetching rooms:', error)
     });
