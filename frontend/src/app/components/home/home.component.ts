76ce6cb298ac5fe4641376c55c5d9ea31952e9e8
mohammedAljadd <aljadd3@gmail.com>
2025-04-06 04:32:17 +0200

    hide register button if already logged in

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit{
   
+  token!: string | null;
+
+  ngOnInit(): void {
+      this.token = localStorage.getItem('jwtToken'); 
+  }
   
   
 
   
 }
+
